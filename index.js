e995ec81e5749002718baaeb102fd50f9ff1ce09
rafael loiola dos santos <manga_loiola@hotmail.com>
2025-04-09 10:11:24 -0300

    estrutura de repeticao

--- index.js
+++ index.js
@@ -28,6 +28,23 @@ const numero = 5
 
 for (let i = 1; i <= 10; i++){
   console.log(5,'x',i,'=',i*numero)
+ 
+}
+console.log('--------------------')
+}
+{
+// 2) Crie um programa que seja capaz de percorrer uma lista de numeros e imprima cada numero par encontrado
+
+const numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+
+
+for (let i = 0; i <numeros.length; i++){
+ const numero = numeros[i];
+
   
+  if (numero % 2 === 0){
+	console.log(numero);
+}
 }
 }
