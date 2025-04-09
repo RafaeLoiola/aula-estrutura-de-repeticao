02b763235e52dfa8ac46890acd43f9583ae34260
rafael loiola dos santos <manga_loiola@hotmail.com>
2025-04-09 09:42:50 -0300

    estrutura de repeticao

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-
-const notas = [];
+{
+//Some todas as notas de um aluno e imprima sua media
+   const notas = [];
 
 notas.push(5);
 notas.push(7);
@@ -16,4 +17,17 @@ for (let index = 0; index < notas.length; index++){
 }
 
 const media = soma / notas.length;
-console.log(media);
+console.log('nota media de Guilherme é',media.toFixed(2));
+
+console.log('----------------')
+}
+{
+   // 1) Crie um programa que dado um numero imprima a sua tabuada.
+
+const numero = 5 
+
+for (let i = 1; i <= 10; i++){
+  console.log(5,'x',i,'=',i*numero)
+  
+}
+}
